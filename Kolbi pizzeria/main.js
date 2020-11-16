326da64f4a1478b8725da6ec9b45e63926e48970
mrsng <70949926+mrsng@users.noreply.github.com>
2020-11-17 00:41:07 +0100

    Add files via upload

---
+++ Kolbi pizzeria/main.js
@@ -0,0 +1,25 @@
+let penz = 0;
+function Ar1(){
+    penz +=2000;
+    document.getElementById("kosarid").innerHTML = "Kosár: " +penz + "Ft";
+}
+function Ar2(){
+    penz +=1100;
+    document.getElementById("kosarid").innerHTML = "Kosár: "+penz + "Ft";
+}
+function Ar3(){
+    penz +=1400;
+    document.getElementById("kosarid").innerHTML = "Kosár: "+penz + "Ft";
+}
+function Ar4(){
+    penz +=1;
+    document.getElementById("kosarid").innerHTML = "Kosár: "+penz + "Ft";
+}
+function Ar5(){
+    penz +=1800;
+    document.getElementById("kosarid").innerHTML = "Kosár: "+penz + "Ft";
+}
+function Ar6(){
+    penz +=10000;
+    document.getElementById("kosarid").innerHTML = "Kosár: "+penz + "Ft";
+}
